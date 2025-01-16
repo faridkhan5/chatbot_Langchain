0632554eb5c77ea2852b82e37c4d5cff4a0b9d76
faridkhan5 <faridk302@gmail.com>
2025-01-16 10:52:01 +0530

    FastAPI server for essay and poem generation using LangChain

---
+++ api/client.py
@@ -0,0 +1,31 @@
+import requests
+import streamlit as st
+
+def get_openai_response(input_text):
+    response = requests.post(
+        "http://localhost:8000/essay/invoke",
+        json={'input': {'topic': input_text}}
+    )
+    print('x---------response----------------x')
+    print(response.text)
+    print('x-------------------------x')
+
+    return response.json()['output']['content']
+
+def get_ollama_response(input_text):
+    response = requests.post(
+        "http://localhost:8000/poem/invoke",
+        json={'input': {'topic': input_text}}
+    )
+
+    return response.json()['output']
+
+
+st.title('Langchain Demo with Llama2 API')
+input_text1 = st.text_input("Write an essay on")
+input_text2 = st.text_input("Write a poem on")
+
+if input_text1:
+    st.write(get_openai_response(input_text1))
+if input_text2:
+    st.write(get_ollama_response(input_text2))
